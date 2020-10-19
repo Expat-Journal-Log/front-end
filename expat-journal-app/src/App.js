374673c555220b03d64b196a948cf1aba456e005
Darren Tebo <darrentebo83@outlook.com>
2020-10-19 15:57:07 -0400

    created initial axios get call and data render

--- expat-journal-app/src/App.js
+++ expat-journal-app/src/App.js
@@ -1,33 +1,54 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Login from './components/Login';
 import Posts from './components/Posts';
 import Header from './components/Header';
 import PostForm from './components/PostForm';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { ContextObject } from './context/context';
+
+const initialState = {
+  posts: [],
+  error: ''
+};
 
 function App() {
+  const [posts, setPosts] = useState(initialState.posts);
+
+  useEffect(() => {
+    axiosWithAuth()
+      .get(`/posts`)
+      .then(res => {
+        console.log('App: useEffect: DT: ', res);
+
+        setPosts(res.data);
+      })
+      .catch(err => console.error('App: useEffect: DT: Error: ', err));
+  }, []);
+
   return (
     <Router>
-    <div className="App">
-   {/* <h1>Expat Journal</h1>
-   <h2>Login</h2> */}
-    <Header />
+      <ContextObject.Provider>
+        <div className="App">
+          {/* <h1>Expat Journal</h1>
+          <h2>Login</h2> */}
+            <Header />
 
-    <Route exact path='/posts'>
-    <Posts /> 
-    </Route>
+            <Route exact path='/posts'>
+            <Posts /> 
+            </Route>
 
-    <Route exact path='/create-post'>
-    <PostForm /> 
-    </Route>
+            <Route exact path='/create-post'>
+            <PostForm /> 
+            </Route>
 
-    <Route exact path='/'>
-    <Login /> 
-    </Route>
+            <Route exact path='/'>
+            <Login /> 
+            </Route>
 
 
-    </div>
+          </div>
+        </ContextObject.Provider>
     </Router>
   );
 }
--- expat-journal-app/src/components/Posts.js
+++ expat-journal-app/src/components/Posts.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react'; 
+import { ContextObject } from '../context/context';
 import HeaderPostButton from './HeaderPostButton';
-
+import Post from './Post';
 
 const Posts = () => {
+    const { posts } = useContext(ContextObject);
     return(
         <>
             <HeaderPostButton />
-            <h1>Posts here...</h1>
+            {posts.map(post => {
+                <Post key={post.id} post={post} />
+            })}
             <div className='image-container'>
             <img src='https://images.unsplash.com/photo-1585567512124-dbfaa0e7eee5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=80' />
             <img src='https://images.unsplash.com/photo-1447755086558-cb9e3830d677?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80' />
