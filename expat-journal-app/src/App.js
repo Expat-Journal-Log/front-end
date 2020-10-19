e907e3e547711cb99ef13781b0ec9c971e152054
Darren Tebo <darrentebo83@outlook.com>
2020-10-19 16:45:41 -0400

    changes

--- expat-journal-app/src/App.js
+++ expat-journal-app/src/App.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import './App.css';
-<<<<<<< HEAD
-=======
 import Login from './components/Login';
 import Posts from './components/Posts';
 import Header from './components/Header';
 import PostForm from './components/PostForm';
->>>>>>> 2dfdecd44fb6029fa351a26a52346153c183b6df
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Login from './components/Login';
 import Register from './components/Register';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
 <<<<<<< HEAD
