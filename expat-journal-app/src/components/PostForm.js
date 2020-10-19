df69a85b68d574d82713454c14e36b937dd82543
Darren Tebo <darrentebo83@outlook.com>
2020-10-19 14:51:34 -0400

    created post form

--- expat-journal-app/src/components/PostForm.js
+++ expat-journal-app/src/components/PostForm.js
@@ -1,9 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+const initialValues = {
+    title = '',
+    body = '',
+    imageUrl = ''
+};
 
 const PostForm = () => {
+    const [formValues, setFormValues] = useState(initialValues);
+
+    handleChanges = e => {
+        setFormValues({
+            ...formValues,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    handleSubmit = e => {
+        e.preventDefault();
+    };
+
     return(
         <>
-            <h1>PostForm goes here...</h1>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor='title'>Title</label>
+                <input
+                    type='text'
+                    name='title'
+                    id='title'
+                    value={formValues.title}
+                    onChange={handleChanges}
+                />
+                <label htmlFor='body'>Body</label>
+                <textarea
+                    name='body'
+                    id='body'
+                    value={formValues.body}
+                    onChange={handleChanges}
+                />
+                <label htmlFor='imageUrl'>Image</label>
+                <input
+                    type='text'
+                    id='imageUrl'
+                    value={formValues.imageUrl}
+                    onChange={handleChanges}
+                />
+                <button>Add Post</button>
+            </form>
         </>
     );
 };
