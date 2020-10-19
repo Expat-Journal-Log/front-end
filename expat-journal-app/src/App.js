182ab102f8004c3741b30504a27b94c0d25d05b2
TJ Janus <TJJanus1@gmail.com>
2020-10-19 12:53:45 -0400

    started login form and styling

---
+++ expat-journal-app/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import Login from './components/Login';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+    <div className="App">
+   {/* <h1>Expat Journal</h1>
+   <h2>Login</h2> */}
+
+    <Route exact path='/'>
+    <Login /> 
+    </Route>
+
+    </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ expat-journal-app/src/components/Login.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import '../App';
+
+import { Card, CardContent, CardMedia, CardHeader, Typography, Container} from '@material-ui/core';
+function Login() {
+    return (
+        <div className='page'>
+        <div className='container'>
+            <Container>
+            <Card>
+                <CardContent>
+              <Typography variant='h2' component='h2'>Expat Journal</Typography>
+              <Typography variant='h3' component='h3' gutterBottom >Login</Typography>
+              
+
+            <CardMedia 
+            component='img'
+            image='https://images.unsplash.com/photo-1534445867742-43195f401b6c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1300&q=80'
+            height='100'
+            />
+                
+                    
+            <form>
+                <input type='username' name='username'  />
+                <input type='password' name='password'  />
+                <br></br>
+                <button>Login</button>
+                <button>Register</button> 
+
+            </form>
+            </CardContent>
+            </Card>
+            </Container>
+        </div>
+        </div>
+    )
+}
+
+export default Login;
