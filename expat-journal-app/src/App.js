a0795e1a2bcf45ac41adfeb1dc3fc8050c95082d
Darren Tebo <darrentebo83@outlook.com>
2020-10-19 16:48:24 -0400

    added axios fixes

--- expat-journal-app/src/App.js
+++ expat-journal-app/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Login from './components/Login';
 import Posts from './components/Posts';
@@ -6,45 +6,52 @@ import Header from './components/Header';
 import PostForm from './components/PostForm';
 import Register from './components/Register';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { ContextObject } from './context/context';
+
+import axiosWithAuth from './utils/axiosWithAuth';
+
+const initialState = {
+  posts: [],
+  error: ''
+};
 
 function App() {
-<<<<<<< HEAD
-	return (
-		<Router>
-			<div className='App'>
-				<Route exact path='/'>
-					{/* <Login />  */}
-					<Register />
-				</Route>
-			</div>
-		</Router>
-	);
-=======
+  const [posts, setPosts] = useState(initialState.posts);
 
+  useEffect(() => {
+    axiosWithAuth()
+      .get(`/posts`)
+      .then(res => {
+        console.log('App: useEffect: DT: ', res);
+
+        setPosts(res.data);
+      })
+      .catch(err => console.error('App: useEffect: DT: Error: ', err));
+  }, []);
 
   return (
     <Router>
-    <div className="App">
-   {/* <h1>Expat Journal</h1>
-   <h2>Login</h2> */}
-    <Header />
+      <ContextObject.Provider>
+        <div className="App">
+          {/* <h1>Expat Journal</h1>
+          <h2>Login</h2> */}
+            <Header />
 
-    <Route exact path='/posts'>
-    <Posts /> 
-    </Route>
+            <Route exact path='/posts'>
+            <Posts /> 
+            </Route>
 
-    <Route exact path='/create-post'>
-    <PostForm /> 
-    </Route>
+            <Route exact path='/create-post'>
+            <PostForm /> 
+            </Route>
 
-    <Route exact path='/'>
-    <Login /> 
-    </Route>
-
-    </div>
+            <Route exact path='/'>
+            <Login /> 
+            </Route>
+        </div>
+      </ContextObject.Provider>
     </Router>
   );
->>>>>>> 2dfdecd44fb6029fa351a26a52346153c183b6df
 }
 
 export default App;
