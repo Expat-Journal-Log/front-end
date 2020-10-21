661692e270a61be68c8709bfe48a7d0f31caac1f
Darren Tebo <darrentebo83@outlook.com>
2020-10-21 15:25:18 -0400

    styled post cards

--- expat-journal-app/src/components/Post.js
+++ expat-journal-app/src/components/Post.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import { Card, CardContent, CardHeader, Typography } from '@material-ui/core';
 
 const Post = (props) => {
     const { post } = props;
 
     return(
         <>
-            <div className='post'>
+            <Card className='post'>
                 <h2>{post.title}</h2>
                 <img src={`${post.imageURL}`} className={`${post.imageURL ? '':'hide'}`}/>
                 <p>{post.description}</p>
-            </div>
+            </Card>
         </>
     );
 };
