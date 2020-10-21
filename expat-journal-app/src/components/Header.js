3eaa72539306db41845d28a17d46245d7471e9c7
Darren Tebo <darrentebo83@outlook.com>
2020-10-21 15:36:57 -0400

    updated card styles

--- expat-journal-app/src/components/Header.js
+++ expat-journal-app/src/components/Header.js
@@ -7,7 +7,7 @@ function Header() {
         <div className={`${(window.location.pathname === '/' || window.location.pathname === '/register') ? 'hide' : ''}`}>
             <header className='header'>
                 <img src={logo} />
-                
+                <h2>Expat Journal</h2>
             </header>
         </div>
     )
