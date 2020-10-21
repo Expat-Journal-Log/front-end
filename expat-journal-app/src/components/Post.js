9ad806392aba3845658818d7873f0e835371bc2a
Jake Grella <jgrella13@gmail.com>
2020-10-21 13:47:09 -0500

    restructuring styles

--- expat-journal-app/src/components/Post.js
+++ expat-journal-app/src/components/Post.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import HeaderPostButton from './HeaderPostButton';
+import useStyles from '../styles/postStyle';
 
 const Post = (props) => {
-    const { post } = props;
+	const classes = useStyles();
+	const { post } = props;
 
-    return(
-        <>
-            <div className='post'>
-                <h2>{post.title}</h2>
-                <img src={`${post.imageURL}`} className={`${post.imageURL ? '':'hide'}`}/>
-                <p>{post.description}</p>
-            </div>
-        </>
-    );
+	return (
+		<>
+			<div className={classes.post}>
+				<h2 className={classes.postH2}>{post.title}</h2>
+				<img
+					src={`${post.imageURL}`}
+					// hide alt img icon
+					// className={`${post.imageURL ? '' : 'hide'}`}
+					className={classes.postImg}
+				/>
+				<p>{post.description}</p>
+			</div>
+		</>
+	);
 };
 
 export default Post;
--- expat-journal-app/src/components/Posts.js
+++ expat-journal-app/src/components/Posts.js
@@ -1,24 +1,24 @@
-import React, { useContext, useState, useEffect } from 'react'; 
+import React, { useContext, useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { ContextObject } from '../context/context';
 import HeaderPostButton from './HeaderPostButton';
 import Post from './Post';
 
-import './post.css';
+import '../styles/post.css';
 
 const Posts = () => {
-    const data = useContext(ContextObject);
+	const data = useContext(ContextObject);
 
-    return(
-        <div className='post-card'>
-            <HeaderPostButton editing='false' />
-            {data.postState.posts.map(post => (
-                <Link to={`/post/${post.postId}`}>
-                    <Post key={post.postId} post={post} />
-                </Link>
-            ))}
-        </div>
-    );
+	return (
+		<div className='post-card'>
+			<HeaderPostButton editing='false' />
+			{data.postState.posts.map((post) => (
+				<Link to={`/post/${post.postId}`}>
+					<Post key={post.postId} post={post} />
+				</Link>
+			))}
+		</div>
+	);
 };
 
 export default Posts;
