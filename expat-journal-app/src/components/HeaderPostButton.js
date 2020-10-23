2ccd02fab7b13a094caeffe02bc6c694dbbdd85e
Darren Tebo <darrentebo83@outlook.com>
2020-10-23 18:38:11 -0400

    fixed bug where the edit button on the edit page became a menu button on mobile

--- expat-journal-app/src/components/HeaderPostButton.js
+++ expat-journal-app/src/components/HeaderPostButton.js
@@ -3,6 +3,7 @@ import { Link, useParams } from 'react-router-dom'
 import PostAddIcon from '@material-ui/icons/PostAdd';
 import EditRoundedIcon from '@material-ui/icons/EditRounded';
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import MenuIcon from '@material-ui/icons/Menu';
 import { Menu, Button, MenuItem } from '@material-ui/core';
 import '../styles/post.css';
 
@@ -29,24 +30,12 @@ function HeaderPostButton(props) {
         setAnchorEl(null);
     };
 
+    const iconStyles = {
+        fontSize: '7rem'
+    };
+    
     return (
         <>
-            <div className='menu-container'>
-                {/*This is to be used in the mobile view*/}
-                <Button aria-controls='menu' aria-haspopup='true' onClick={handleClick}>
-                    Open Menu
-                </Button>
-                <Menu
-                    id='menu'
-                    anchorEl={anchorEl}
-                    keepMounted
-                    open={Boolean(anchorEl)}
-                    onClose={handleClose}
-                >
-                    <MenuItem onClick={handleClose}>Create Post</MenuItem>
-                    <MenuItem onClick={logout}>Logout</MenuItem>
-                </Menu>
-            </div>
             <div className='flex-container1'>
                 {(editing === 'true' && localStorage.getItem('token')) ? (
                     <>
@@ -59,21 +48,48 @@ function HeaderPostButton(props) {
                     </>
                 ) : (
                     <>
-                        <Link to='/create-post' className='link'>
-                            <div className='buttonStyle' >
-                                
-                        <PostAddIcon fontSize='large' className='createPostButton buttonStyle '>create a post</PostAddIcon>
-                            <p className='createPostText'>Create <br></br>Post</p>
-                            </div>
-                        </Link>
-                        <div>
-                            <ExitToAppIcon
-                            className='logout buttonStyle'
-                            onClick={logout}
+                        <div className='menu-container'>
+                            {/*This is to be used in the mobile view*/}
+                                <Button
+                                    className='menu-button'
+                                    aria-controls='menu'
+                                    aria-haspopup='true'
+                                    onClick={handleClick}
+                                >
+                                    <MenuIcon fontSize='large' style={iconStyles} />
+                                </Button>
+                            <Menu
+                                id='menu'
+                                anchorEl={anchorEl}
+                                keepMounted
+                                open={Boolean(anchorEl)}
+                                onClose={handleClose}
                             >
-                                Logout
-                            </ExitToAppIcon>
-                            <p className='createPostText'>Log <br></br>out</p>
+                                <MenuItem onClick={handleClose}>
+                                    Create Post
+                                </MenuItem>
+                                <MenuItem onClick={logout}>
+                                    Logout
+                                </MenuItem>
+                            </Menu>
+                        </div>
+                        <div className='header-menu'>
+                            <Link to='/create-post' className='link'>
+                                <div className='buttonStyle' >
+                                    
+                            <PostAddIcon fontSize='large' className='createPostButton buttonStyle '>create a post</PostAddIcon>
+                                <p className='createPostText'>Create <br></br>Post</p>
+                                </div>
+                            </Link>
+                            <div>
+                                <ExitToAppIcon
+                                className='logout buttonStyle'
+                                onClick={logout}
+                                >
+                                    Logout
+                                </ExitToAppIcon>
+                                <p className='createPostText'>Log <br></br>out</p>
+                            </div>
                         </div>
                     </>
                 )}
